61fa092cac41b2ae1d9320bd1a969ad10301e85a
Arthur <baginian.artur@yandex.ru>
2024-10-13 21:57:35 +0300

    Changes after tests...

--- swaggerqt/models/media_type.py
+++ swaggerqt/models/media_type.py
@@ -2,9 +2,11 @@ from typing import Any, Optional
 
 from pydantic import BaseModel, Field
 
+from .schema import Schema
+
 
 class MediaType(BaseModel):
-    schema_: Optional[dict[str, str]] = Field(default=None, alias="schema")
+    schema_field: Schema = Field(default=None, serialization_alias="schema")
     example: Optional[Any] = None
     examples: Optional[dict[str, dict[str, Any]]] = None
     encoding: Optional[dict[str, dict[str, Any]]] = None
