e4d26ffda1cdafbd94b3c4a195352c17dba3f43d
Arthur <baginian.artur@yandex.ru>
2024-10-24 14:09:33 +0300

    rename: test_dynamic_creation to test_parser

---
+++ tests/test_parser.py
@@ -0,0 +1,42 @@
+from swaggerqt.models.parsing import JsonParser
+
+
+def test_parser():
+    parser = JsonParser()
+
+    assert (
+        parser.parse_type({"type": "array", "items": {"type": "string"}})
+        == list[str]
+    )
+
+    user_schema = {
+        "type": "object",
+        "properties": {
+            "id": {
+                "title": "Id",
+                "type": "integer",
+            },
+            "name": {
+                "title": "Name",
+                "type": "string",
+                "example": "Arthur",
+            },
+            "age": {
+                "title": "Age",
+                "type": "integer",
+            },
+        },
+        "required": [
+            "id",
+            "name",
+            "age",
+        ],
+    }
+    User = parser.parse_json_schema("User", user_schema)
+    user_array_schema = {
+        "type": "array",
+        "items": {
+            "type": "User",
+        },
+    }
+    assert parser.parse_type(user_array_schema) == list[User]
