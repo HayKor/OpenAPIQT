24cb7c947d018d1588c7d6eee10ebf23086eed45
Arthur <baginian.artur@yandex.ru>
2024-11-14 21:34:05 +0300

    Add schema manager + UI

---
+++ swaggerqt/app/widgets/property_field.py
@@ -0,0 +1,40 @@
+from typing import Callable
+
+from PyQt6.QtWidgets import (
+    QCheckBox,
+    QHBoxLayout,
+    QLineEdit,
+    QPushButton,
+    QWidget,
+)
+
+
+class PropertyField(QWidget):
+    def __init__(self, remove_callback: Callable):
+        super().__init__()
+
+        # Create layout for the property field
+        layout = QHBoxLayout()
+
+        # Create input fields for property name and type
+        self.property_name_input = QLineEdit()
+        self.property_type_input = QLineEdit()
+        self.is_required_checkbox = QCheckBox("*")
+        self.destroy_btn = QPushButton("X")
+
+        #
+        self.destroy_btn.setFixedSize(20, 20)
+
+        # Set placeholder text
+        self.property_name_input.setPlaceholderText("Property Name")
+        self.property_type_input.setPlaceholderText("Property Type")
+
+        self.destroy_btn.clicked.connect(remove_callback)
+
+        # Add inputs to layout
+        layout.addWidget(self.property_name_input)
+        layout.addWidget(self.property_type_input)
+        layout.addWidget(self.is_required_checkbox)
+        layout.addWidget(self.destroy_btn)
+
+        self.setLayout(layout)
---
+++ swaggerqt/app/widgets/schema_manager.py
@@ -0,0 +1,29 @@
+from app.ui.schema_manager_ui import Ui_SchemaManager
+from app.widgets.property_field import PropertyField
+from PyQt6.QtWidgets import QWidget
+
+
+class SchemaManager(QWidget, Ui_SchemaManager):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        # Initial field
+        self.add_property_field()
+
+        # Connect when clicked
+        self.add_property_btn.clicked.connect(self.add_property_field)
+
+    def add_property_field(self):
+        """Add a new property field."""
+        property_field = PropertyField(self.remove_property_field)
+        self.property_container.addWidget(property_field)
+
+    def remove_property_field(self):
+        if len(self.property_container) == 2:  # because 1 is frame ig
+            return
+        sender = self.sender()
+        property_field = sender.parent()  # type: ignore
+        if isinstance(property_field, QWidget):
+            self.property_container.removeWidget(property_field)
+            property_field.deleteLater()
