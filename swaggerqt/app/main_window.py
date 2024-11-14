b96c87ae95465936973baa2bba2baa143850be80
Arthur <baginian.artur@yandex.ru>
2024-11-14 17:41:12 +0300

    Create `MainWindow`

--- swaggerqt/app/main_window.py
+++ swaggerqt/app/main_window.py
@@ -1,20 +1,15 @@
 import logging
 
-from app.generate_yaml import GenerateYAML
-from app.ui.main_window_ui import Ui_MainWindow
-from models.parsing import JsonParser
-from PyQt6.QtWidgets import QWidget
+from app.widgets.main_widget import MainWidget
+from PyQt6.QtWidgets import QMainWindow
 
 
-class MainWindow(QWidget, Ui_MainWindow):
-    def __init__(self):
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
         super().__init__()
-        self.setupUi(self)
-        self.parser = JsonParser()
-        self.generate_yaml_window = GenerateYAML()
-        self.generate_yaml_btn.clicked.connect(self.on_click_generate_yaml)
+        self.setWindowTitle("PyQT Swagger Builder")
+
+        main_widget = MainWidget()
+        self.setCentralWidget(main_widget)
 
         logging.debug("Window '%s' has initialized", self.__class__.__name__)
-
-    def on_click_generate_yaml(self):
-        self.generate_yaml_window.show()
