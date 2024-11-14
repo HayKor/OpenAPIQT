0c7ebf28a369e829717fbd78cf1e659b29eb1333
Arthur <baginian.artur@yandex.ru>
2024-11-14 21:34:16 +0300

    Use schema manager

--- swaggerqt/app/widgets/main_widget.py
+++ swaggerqt/app/widgets/main_widget.py
@@ -2,6 +2,7 @@ import logging
 
 from app.ui.main_widget_ui import Ui_MainWidget
 from app.widgets.generate_yaml import GenerateYAML
+from app.widgets.schema_manager import SchemaManager
 from models.parsing import JsonParser
 from PyQt6.QtWidgets import QWidget
 
@@ -12,7 +13,11 @@ class MainWidget(QWidget, Ui_MainWidget):
         self.setupUi(self)
 
         self.parser = JsonParser()
+
         self.generate_yaml_window = GenerateYAML()
+        self.manage_schemas_btn.clicked.connect(self.on_click_manage_schemas)
+
+        self.schema_manager_window = SchemaManager()
         self.generate_yaml_btn.clicked.connect(self.on_click_generate_yaml)
 
         logging.debug("Widget '%s' has initialized", self.__class__.__name__)
@@ -20,3 +25,6 @@ class MainWidget(QWidget, Ui_MainWidget):
     def on_click_generate_yaml(self):
         # TODO: actually implement some logic
         self.generate_yaml_window.show()
+
+    def on_click_manage_schemas(self):
+        self.schema_manager_window.show()
